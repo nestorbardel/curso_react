7d5194a452781061c9dbe7655cf3b6ab4d673d5d
Nestor <nestorbardel@gmail.com>
2021-12-01 09:26:38 -0300

    active

--- 07-heroes-app/src/components/ui/Navbar.js
+++ 07-heroes-app/src/components/ui/Navbar.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const handleLogout = () => {
+        //TODO:Por hacer
+        console.log('logout');
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -16,7 +22,6 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink 
-                        activeClassName="active"
                         className="nav-item nav-link" 
                         exact
                         to="/marvel"
@@ -35,16 +40,15 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
+                    <span className="nav-item nav-link text-info">Nestor</span>
+                    <button 
+                        className="nav-item nav-link btn" 
+                        onClick={handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
